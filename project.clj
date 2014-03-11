013875831095a0a8c570681d15688056b65c27ec
jules <jules_gosnell@yahoo.com>
2014-03-11 20:18:21 +0000

    upgrade clojure

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.6.0-beta2"]
+                 [org.clojure/clojure "1.6.0-RC1"]
                  ;;[org.clojure/clojure "1.6.0-master-SNAPSHOT"]
                  [org.clojure/core.rrb-vector "0.0.10"]
                  [com.amd/okra-with-sim "1.8"]
