f3d0e8776153bcfb3a6ad545a5566935245be9bb
jules <jules_gosnell@yahoo.com>
2014-03-16 20:41:09 +0000

    test a number of different shaped kernels - exercising statuc and virtual method calls :-)

--- test/clumatra/core_test.clj
+++ test/clumatra/core_test.clj
@@ -1,11 +1,14 @@
 (ns clumatra.core-test
+  (:import  [java.lang.reflect Method])
   (:require [clojure.test :refer :all]
             [clojure.core [reducers :as r]]
             [clumatra.core :refer :all]))
 
 (set! *warn-on-reflection* true)
 
-(deftest a-test
+;;------------------------------------------------------------------------------
+
+(deftest object-test
   (testing "copy object array"
     (let [n 32
           kernel-fn (do (println "compiling kernel...")
@@ -24,3 +27,57 @@
             (println "kernel run"))
         (is (= (seq in) (seq out))))
       )))
+
+;;------------------------------------------------------------------------------
+
+(defn find-method [object ^String name]
+  (first (filter (fn [^Method method] (= (.getName method) "invoke")) (.getDeclaredMethods (class object)))))
+
+;;------------------------------------------------------------------------------
+
+(definterface IntKernel (^void invoke [^ints in ^ints out ^int gid]))
+
+(deftest int-test
+  (testing "increment an int array"
+    (let [n 32
+          kernel (reify IntKernel
+                   (^void invoke [^IntKernel self ^ints in ^ints out ^int gid]
+                     (aset out gid (clojure.lang.Numbers/unchecked-inc (aget in gid)))))]
+      (is (=
+           (seq
+            ((kernel-compile2 kernel (find-method kernel "invoke") n)
+             (int-array (range n))
+             (int-array n)))
+           (range 1 (inc n)))))))
+
+;;------------------------------------------------------------------------------
+
+(definterface LongKernel (^void invoke [^longs in ^longs out ^int gid]))
+
+(deftest long-test
+  (testing "increment an long array"
+    (let [n 32
+          kernel (reify LongKernel
+                   (^void invoke [^LongKernel self ^longs in ^longs out ^int gid]
+                     (aset out gid (clojure.lang.Numbers/unchecked-inc (aget in gid)))))]
+      (is (=
+           (seq
+            ((kernel-compile2 kernel (find-method kernel "invoke") n)
+             (long-array (range n))
+             (long-array n)))
+           (range 1 (inc n)))))))
+
+;;------------------------------------------------------------------------------
+
+(definterface StringIntKernel (^void invoke [^"[Ljava.lang.String;" in ^ints out ^int gid]))
+
+(deftest string-int-test
+  (testing "find length of an array of Strings"
+    (let [n 32
+          kernel (reify StringIntKernel
+                   (^void invoke [^StringIntKernel self ^"[Ljava.lang.String;" in ^ints out ^int gid]
+                     (aset out gid (.length ^String (aget in gid)))))]
+      (is (= (seq ((kernel-compile2 kernel (find-method kernel "invoke") n)
+                   (into-array ^String (map (fn [^Long i] (.toString i)) (range n)))
+                   (int-array n)))
+             (map (fn [^Long i] (.length (.toString i))) (range n)))))))
