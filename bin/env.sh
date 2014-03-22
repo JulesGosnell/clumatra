ab977cf91f88f6daad77336a1d97dbad78ca2e75
jules <jules_gosnell@yahoo.com>
2014-03-22 23:45:17 +0000

    mvn test almost working but tests all breaking on : Caused by: java.lang.ClassCastException: com.oracle.graal.hotspot.hsail.HSAILHotSpotLIRGenerator cannot be cast to com.oracle.graal.hotspot.HotSpotLIRGenerator  at com.oracle.graal.hotspot.nodes.PrefetchAllocateNode.generate (PrefetchAllocateNode.java:46) ??

--- bin/env.sh
+++ bin/env.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 export JAVA_HOME=`pwd`/graal/graal/jdk1.8.0-internal/product
 export JAVACMD=$JAVA_HOME/bin/java
 export PATH=$JAVA_HOME/bin:$PATH
-
+export MAVEN_OPTS="-Xms1g -Xmx1g -server -XX:+GPUOffload -XX:+TraceGPUInteraction"
 $@
