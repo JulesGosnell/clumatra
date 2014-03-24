b06473de338fde40ee6f266732ca9c50a6f491c3
jules <jules_gosnell@yahoo.com>
2014-03-24 21:16:41 +0000

    comment out three tests that do not compile to HSAIL

--- test/clumatra/core_test.clj
+++ test/clumatra/core_test.clj
@@ -332,38 +332,45 @@
 
 (definterface ObjectBooleanKernel (^void invoke [^"[Ljava.lang.Object;" in ^booleans out ^int gid]))
 
-(deftest isZero-test
-  (testing "apply static java function to elements of Object[]"
-    (let [n 32
-          kernel (reify ObjectBooleanKernel
-                   (invoke [self in out gid]
-                     (aset out gid (clojure.lang.Numbers/isZero (aget in gid)))))]
-      (is (test-kernel
-           kernel (find-method kernel "invoke") n
-           (into-array ^Object (range 32))
-           (boolean-array n))))))
+;; com.oracle.graal.graph.GraalInternalError: unimplemented
+;;
+;; (deftest isZero-test
+;;   (testing "apply static java function to elements of Object[]"
+;;     (let [n 32
+;;           kernel (reify ObjectBooleanKernel
+;;                    (invoke [self in out gid]
+;;                      (aset out gid (clojure.lang.Numbers/isZero (aget in gid)))))]
+;;       (is (test-kernel
+;;            kernel (find-method kernel "invoke") n
+;;            (into-array ^Object (range 32))
+;;            (boolean-array n))))))
 
-(deftest isPos-test
-  (testing "apply static java function to elements of Object[]"
-    (let [n 32
-          kernel (reify ObjectBooleanKernel
-                   (invoke [self in out gid]
-                     (aset out gid (clojure.lang.Numbers/isPos (aget in gid)))))]
-      (is (test-kernel
-           kernel (find-method kernel "invoke") n
-           (into-array ^Object (range -16 16))
-           (boolean-array n))))))
+;; com.oracle.graal.graph.GraalInternalError: unimplemented
+;;
+;; (deftest isPos-test
+;;   (testing "apply static java function to elements of Object[]"
+;;     (let [n 32
+;;           kernel (reify ObjectBooleanKernel
+;;                    (invoke [self in out gid]
+;;                      (aset out gid (clojure.lang.Numbers/isPos (aget in gid)))))]
+;;       (is (test-kernel
+;;            kernel (find-method kernel "invoke") n
+;;            (into-array ^Object (range -16 16))
+;;            (boolean-array n))))))
 
-(deftest isNeg-test
-  (testing "apply static java function to elements of Object[]"
-    (let [n 32
-          kernel (reify ObjectBooleanKernel
-                   (invoke [self in out gid]
-                     (aset out gid (clojure.lang.Numbers/isNeg (aget in gid)))))]
-      (is (test-kernel
-           kernel (find-method kernel "invoke") n
-           (into-array ^Object (range -16 16))
-           (boolean-array n))))))
+
+;; com.oracle.graal.graph.GraalInternalError: unimplemented
+;;
+;; (deftest isNeg-test
+;;   (testing "apply static java function to elements of Object[]"
+;;     (let [n 32
+;;           kernel (reify ObjectBooleanKernel
+;;                    (invoke [self in out gid]
+;;                      (aset out gid (clojure.lang.Numbers/isNeg (aget in gid)))))]
+;;       (is (test-kernel
+;;            kernel (find-method kernel "invoke") n
+;;            (into-array ^Object (range -16 16))
+;;            (boolean-array n))))))
 
 ;;------------------------------------------------------------------------------
 
