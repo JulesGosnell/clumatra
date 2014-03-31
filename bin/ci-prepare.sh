dea80b3f85e297ca3f728153bfb84f80d9e0ecc4
Jules Gosnell <jules_gosnell@yahoo.com>
2014-04-01 00:32:24 +0100

    do relevant setup for each matrix build

--- bin/ci-prepare.sh
+++ bin/ci-prepare.sh
@@ -5,8 +5,18 @@ wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/jdk1.8.0-graal.tgz | tar 
 rm -fr lib
 mkdir lib
 cd lib
-wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libamdhsacl64.so
-wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libnewhsacore64.so
-wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libokra_x86_64.so
+
+case "$1" in
+    no-acceleration)
+	;;
+    simulated-acceleration)
+	;;
+    hardware-acceleration)
+	wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libamdhsacl64.so
+	wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libnewhsacore64.so
+	wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libokra_x86_64.so
+	;;
+esac
+
 cd ..
 
