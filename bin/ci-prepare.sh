8cf919fa89b1ebfeb4493a25ae0247185012d77e
Jules Gosnell <jules_gosnell@yahoo.com>
2014-04-01 00:19:21 +0100

    remove jdk and lib before downloading fresh copies

--- bin/ci-prepare.sh
+++ bin/ci-prepare.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 
+rm -fr jdk1.8.0-graal
 wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/jdk1.8.0-graal.tgz | tar zxf -
+rm -fr lib
 mkdir lib
 cd lib
 wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libamdhsacl64.so
