5d2780dc65586f52337c9dffc81a7345bd487834
Jules Gosnell <jules_gosnell@yahoo.com>
2014-04-02 23:35:44 +0100

    fix JAVA_HOME

--- bin/env.sh
+++ bin/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME=`pwd`/graal/graal/jdk1.8.0-internal/product
+export JAVA_HOME=`pwd`/jdk1.8.0-graal/
 export JAVACMD=$JAVA_HOME/bin/java
 export PATH=$JAVA_HOME/bin:$PATH
 export MAVEN_OPTS="-Xms1g -Xmx1g -server -XX:+GPUOffload -XX:+TraceGPUInteraction"
