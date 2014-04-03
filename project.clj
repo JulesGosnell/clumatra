83bfe5b0225512133ec2ea36807f5ee56d5a80f6
jules <jules_gosnell@yahoo.com>
2014-04-04 00:13:57 +0100

    add Eric's flags

--- project.clj
+++ project.clj
@@ -18,5 +18,5 @@
 
   :jvm-opts ["-Xms1g" "-Xmx1g" "-server"
              ;;"-Dclumatra.verbose=true"
-             "-XX:+GPUOffload" "-XX:+TraceGPUInteraction"]
+             "-G:Log=CodeGen -XX:+GPUOffload" "-XX:+TraceGPUInteraction -XX:-UseHSAILDeoptimization"]
   )
