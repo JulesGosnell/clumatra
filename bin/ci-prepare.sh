f83222a105d3930fd55af386eb13a3fcb2ea2cdb
Jules Gosnell <jules_gosnell@yahoo.com>
2014-04-03 23:38:29 +0100

    we'll build the no-acceleration build on the same vm - excludes i386 boxes at the moment

--- bin/ci-prepare.sh
+++ bin/ci-prepare.sh
@@ -1,15 +1,14 @@
 #!/bin/sh -x
 
 rm -fr jdk1.8.0-graal lib
+wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/jdk1.8.0-graal.tgz | tar zxf -
 
 case "$1" in
     no-acceleration)
 	;;
     simulated-acceleration)
-	wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/jdk1.8.0-graal.tgz | tar zxf -
 	;;
     hardware-acceleration)
-	wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/jdk1.8.0-graal.tgz | tar zxf -
 	mkdir lib
 	cd lib
 	wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libamdhsacl64.so
