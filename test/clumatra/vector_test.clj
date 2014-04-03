b514484e30e134f8f32d9d3c51d0236780b9c686
Jules Gosnell <jules_gosnell@yahoo.com>
2014-04-03 21:14:30 +0100

    lose some timings temporarily

--- test/clumatra/vector_test.clj
+++ test/clumatra/vector_test.clj
@@ -38,11 +38,11 @@
     (is (test-foo (+ 33 (* 32 32 32 32)) foo))
     ))
 
-(deftest exercise-backends
-  (testing "backends"
-    (is (test-foo2 :sequential        :sequential 33 identity))
-    (is (test-foo2 :threads-parallel  :sequential 33 identity))
-    ))
+;; (deftest exercise-backends
+;;   (testing "backends"
+;;     (is (test-foo2 :sequential        :sequential 33 identity))
+;;     (is (test-foo2 :threads-parallel  :sequential 33 identity))
+;;     ))
 
 ;; TODO:
 ;; look at core reducer
@@ -51,28 +51,28 @@
 ;; write some tests that make use of Map splicing
 ;; consider moving reduce/combine to seqspert, with an optional clumatra binding
 
-(def a (into [] (range 10000000)))
-(time (do (into [] a) nil)) ;; 124
-(time (do (mapv identity a) nil)) ;; 220
-(time (do (vmap identity a) nil)) ;; 140
-(time (do (mapv inc a) nil)) ;; 280
-(time (do (vmap inc a) nil)) ;; 240
+;; (def a (into [] (range 10000000)))
+;; (time (do (into [] a) nil)) ;; 124
+;; (time (do (mapv identity a) nil)) ;; 220
+;; (time (do (vmap identity a) nil)) ;; 140
+;; (time (do (mapv inc a) nil)) ;; 280
+;; (time (do (vmap inc a) nil)) ;; 240
 
-;; vmap should win when used in parallel mode because of the zero cost
-;; of concatenation. - but current reduction code not suitable
+;; ;; vmap should win when used in parallel mode because of the zero cost
+;; ;; of concatenation. - but current reduction code not suitable
 
-(def n (/ (count a) (.availableProcessors (Runtime/getRuntime)))) ;; = 625000
+;; (def n (/ (count a) (.availableProcessors (Runtime/getRuntime)))) ;; = 625000
 
-(do (time (r/fold (r/monoid into vector) conj a)) nil) ;; 380 ms
-(do (time (r/fold (r/monoid v/catvec v/vector) conj a)) nil) ;; 380 ms
+;; (do (time (r/fold (r/monoid into vector) conj a)) nil) ;; 380 ms
+;; (do (time (r/fold (r/monoid v/catvec v/vector) conj a)) nil) ;; 380 ms
 
-(do (time (r/fold (r/monoid into vector) conj (r/map inc a))) nil) ;; 620 ms
-(do (time (r/fold (r/monoid v/catvec v/vector) conj (r/map inc a))) nil) ;; 680 ms
+;; (do (time (r/fold (r/monoid into vector) conj (r/map inc a))) nil) ;; 620 ms
+;; (do (time (r/fold (r/monoid v/catvec v/vector) conj (r/map inc a))) nil) ;; 680 ms
 
-(do (time (r/fold n (r/monoid into vector) conj (r/map inc a))) nil) ;; 590 ms
-(do (time (r/fold n (r/monoid v/catvec v/vector) conj (r/map inc a))) nil) ;; 320 - 520 - erratic !
+;; (do (time (r/fold n (r/monoid into vector) conj (r/map inc a))) nil) ;; 590 ms
+;; (do (time (r/fold n (r/monoid v/catvec v/vector) conj (r/map inc a))) nil) ;; 320 - 520 - erratic !
 
-(time (count (vmap inc a))) ;; 230 ms
-(time (count (fjvmap inc a)))
-(= (time (r/fold n (r/monoid v/catvec v/vector) conj (r/map inc a))) (fjvmap inc a))
+;; (time (count (vmap inc a))) ;; 230 ms
+;; (time (count (fjvmap inc a)))
+;; (= (time (r/fold n (r/monoid v/catvec v/vector) conj (r/map inc a))) (fjvmap inc a))
 
