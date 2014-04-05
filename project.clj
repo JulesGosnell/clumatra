39de94cfbc268d1fd197e8492d2c8070aa9f25c6
Jules Gosnell <jules_gosnell@yahoo.com>
2014-04-05 09:00:33 +0100

    switch to hardware acceleration add ouroboros repo for deployed jars

--- project.clj
+++ project.clj
@@ -1,21 +1,27 @@
 (defproject clumatra "0.1.0-SNAPSHOT"
+  
   :description "Clumatra: Clojure for the GPU via Sumatra / Graal / Okra"
+  
   :url "http://git@github.com:JulesGosnell/clumatra"
+  
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
   :dependencies [
                  [org.clojure/clojure "1.6.0"]
                  ;;[org.clojure/clojure "1.6.0-master-SNAPSHOT"]
                  [org.clojure/core.rrb-vector "0.0.10"]
-                 [com.amd/okra-with-sim "1.8"]
+                 [com.amd/okra "1.8"]
                  ]
-
+  
   :plugins [[lein-nodisassemble "0.1.3"]]
-
+  
+  :repositories [["ouroboros" "http://ouroboros.dyndns-free.com/artifactory/repo"]]
+  
   :global-vars {*warn-on-reflection* true
                 *assert* false
                 *unchecked-math* true}
-
+  
   :jvm-opts ["-Xms1g" "-Xmx1g" "-server"
              ;;"-Dclumatra.verbose=true"
              "-G:Log=CodeGen -XX:+GPUOffload" "-XX:+TraceGPUInteraction"]
