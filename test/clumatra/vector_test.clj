7881618a63db8b79422710bd4bc25b2788b7d8ca
jules <jules_gosnell@yahoo.com>
2014-04-23 23:29:10 +0100

    first shot at tests for vector

--- test/clumatra/vector_test.clj
+++ test/clumatra/vector_test.clj
@@ -80,6 +80,15 @@
 
 (def excluded-methods
   #{
+    (.getMethod clojure.lang.AFn "run" (into-array Class []))
+    (.getMethod java.lang.Object "wait" (into-array Class [Long/TYPE Integer/TYPE]))
+    (.getMethod java.lang.Object "wait" (into-array Class [Long/TYPE]))
+    (.getMethod java.lang.Object "wait" (into-array Class []))
+    (.getMethod java.lang.Object "notify" (into-array Class []))
+    (.getMethod java.lang.Object "notifyAll" (into-array Class []))
+    (.getMethod java.lang.Iterable "forEach" (into-array Class [java.util.function.Consumer]))
+    (.getMethod java.util.List "replaceAll" (into-array Class [java.util.function.UnaryOperator]))
+    (.getMethod java.util.List "sort" (into-array Class [java.util.Comparator]))
     ;; vector - unsupported 
     (.getMethod clojure.lang.APersistentVector "removeAll" (into-array Class [java.util.Collection]))
     (.getMethod clojure.lang.APersistentVector "retainAll" (into-array Class [java.util.Collection]))
@@ -91,64 +100,52 @@
     (.getMethod clojure.lang.APersistentVector "addAll" (into-array Class [Integer/TYPE java.util.Collection]))
     (.getMethod clojure.lang.APersistentVector "add" (into-array Class [Object]))
     (.getMethod clojure.lang.APersistentVector "remove" (into-array Class [Object]))
-;;; wierd - a different one from the one in PersistentVector - I cannot find an impl
+    ;; how can I avoid this
     (first (filter (fn [^java.lang.reflect.Method m] (and (= (.getName m) "assoc") (= (.getReturnType m) clojure.lang.Associative))) (.getMethods clojure.lang.APersistentVector))) 
+    (first (filter (fn [^java.lang.reflect.Method m] (and (= (.getName m) "assocN") (= (.getReturnType m) clojure.lang.IPersistentVector))) (.getMethods clojure.lang.PersistentVector))) 
+    (first (filter (fn [^java.lang.reflect.Method m] (and (= (.getName m) "withMeta") (= (.getReturnType m) clojure.lang.IObj))) (.getMethods clojure.lang.PersistentVector))) 
+    (first (filter (fn [^java.lang.reflect.Method m] (and (= (.getName m) "asTransient") (= (.getReturnType m) clojure.lang.ITransientCollection))) (.getMethods clojure.lang.PersistentVector))) 
     ;; looks like iterators do not compare well
     (.getMethod clojure.lang.PersistentVector "iterator" (into-array Class []))
     (.getMethod clojure.lang.APersistentVector "listIterator" (into-array Class [Integer/TYPE]))
     (.getMethod clojure.lang.APersistentVector "listIterator" (into-array Class []))
-;;;(.getMethod clojure.lang.APersistentVector "rangedIterator" (into-array Class [Integer/TYPE Integer/TYPE]))
     (.getMethod clojure.lang.PersistentVector "asTransient" (into-array Class []))
+
+    ;;; NYI
+    (.getMethod clojure.lang.PersistentVector "kvreduce" (into-array Class [clojure.lang.IFn Object]))
+    (.getMethod clojure.lang.PersistentVector "withMeta" (into-array Class [clojure.lang.IPersistentMap]))
+    (.getMethod clojure.lang.PersistentVector "nth" (into-array Class [Integer/TYPE Object]))
+    (.getMethod clojure.lang.APersistentVector "invoke" (into-array Class [Object]))
+    (.getMethod clojure.lang.PersistentVector "create" (into-array Class [(type->array-type Object)]))
+    (.getMethod clojure.lang.PersistentVector "create" (into-array Class [clojure.lang.ISeq]))
+    (.getMethod clojure.lang.PersistentVector "assocN" (into-array Class [Integer/TYPE Object]))
+    (.getMethod clojure.lang.APersistentVector "toArray" (into-array Class [(type->array-type Object)]))
+    (.getMethod clojure.lang.APersistentVector "valAt" (into-array Class [Object]))
+    (.getMethod clojure.lang.PersistentVector "arrayFor" (into-array Class [Integer/TYPE]))
+    (.getMethod clojure.lang.PersistentVector "nth" (into-array Class [Integer/TYPE]))
     })
 
 (def input-fns
-  {
-   (.getMethod clojure.lang.APersistentVector "assoc" (into-array Class [Object Object])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "compareTo" (into-array Class [Object])) [(fn [i] [i])(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "contains" (into-array Class [Object])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "containsAll" (into-array Class [java.util.Collection])) [(fn [i] [i])(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "containsKey" (into-array Class [Object])) [(fn [i] (vec (range (inc i))))]
-;;;   (.getMethod clojure.lang.APersistentVector "doEquals" (into-array Class [clojure.lang.IPersistentVector Object])) [(fn [i] [i])]
-;;;   (.getMethod clojure.lang.APersistentVector "doEquiv" (into-array Class [clojure.lang.IPersistentVector Object])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "entryAt" (into-array Class [Object])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "equals" (into-array Class [Object])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "equiv" (into-array Class [Object])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "get" (into-array Class [Integer/TYPE])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "hashCode" (into-array Class [])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "hasheq" (into-array Class [])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "indexOf" (into-array Class [Object])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "invoke" (into-array Class [Object])) [(fn [i] (vec (range (inc i)))) int]
-   (.getMethod clojure.lang.APersistentVector "isEmpty" (into-array Class [])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "lastIndexOf" (into-array Class [Object])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "length" (into-array Class [])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "peek" (into-array Class [])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "rseq" (into-array Class [])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "size" (into-array Class [])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "subList" (into-array Class [Integer/TYPE Integer/TYPE])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "toArray" (into-array Class [(type->array-type Object)])) [(fn [i] (vec (range (inc i)))) (fn [i](make-array Object (inc i)))]
-   (.getMethod clojure.lang.APersistentVector "toArray" (into-array Class [])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "toString" (into-array Class [])) [(fn [i] [i])]
-   (.getMethod clojure.lang.APersistentVector "valAt" (into-array Class [Object Object])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.APersistentVector "valAt" (into-array Class [Object])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "nth" (into-array Class [Integer/TYPE Object])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "seq" (into-array Class [])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "pop" (into-array Class [])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "count" (into-array Class [])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "meta" (into-array Class [])) [(fn [i] (with-meta (vec (range i)) {:arg i}))]
-   (.getMethod clojure.lang.PersistentVector "withMeta" (into-array Class [clojure.lang.IPersistentMap])) [(fn [i] (vec (range i))) (fn  [i] {:arg i})]
-   (.getMethod clojure.lang.PersistentVector "cons" (into-array Class [Object])) [(fn [i] (vec (range (inc i))))]
-;;   (.getMethod clojure.lang.PersistentVector "nth" (into-array Class [Integer/TYPE])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "arrayFor" (into-array Class [Integer/TYPE])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "chunkedSeq" (into-array Class [])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "assocN" (into-array Class [Integer/TYPE Object])) [(fn [i] (vec (range (inc i))))]
-   (.getMethod clojure.lang.PersistentVector "kvreduce" (into-array Class [clojure.lang.IFn Object])) [(fn [i] (vec (range (inc i)))) (fn [r [k v]] (+ r v))]
-   (.getMethod clojure.lang.PersistentVector "empty" (into-array Class [])) [(fn [i] (vec (range (inc i))))]
-
-   })
+   {
+    (.getMethod clojure.lang.APersistentVector "assoc" (into-array Class [Object Object])) [identity (fn [_] 0) (fn [_] nil)]
+    (.getMethod clojure.lang.APersistentVector "compareTo" (into-array Class [Object])) []
+    (.getMethod clojure.lang.APersistentVector "contains" (into-array Class [Object])) [identity (fn [[h]] h)]
+    (.getMethod clojure.lang.APersistentVector "containsAll" (into-array Class [java.util.Collection])) []
+    (.getMethod clojure.lang.APersistentVector "containsKey" (into-array Class [Object])) [identity (fn [i] 0)]
+    (.getMethod clojure.lang.APersistentVector "entryAt" (into-array Class [Object])) [identity (fn [_] 0)]
+    (.getMethod clojure.lang.APersistentVector "get" (into-array Class [Integer/TYPE])) [identity (fn [_] 0)]
+    (.getMethod clojure.lang.APersistentVector "subList" (into-array Class [Integer/TYPE Integer/TYPE])) [identity (fn [_] 0) (fn [_] 1)]
+    (.getMethod clojure.lang.PersistentVector "meta" (into-array Class [])) [(fn [v] (with-meta v {:arg "meta"}))]
+    }
+  )
 
 (defn non-static? [m] (fn [m] (not (static? m))))
 
-;;;;(deftest-kernels (extract-methods non-static? clojure.lang.PersistentVector excluded-methods) inc input-fns) 
+(deftest-kernels
+  (filter (fn [^java.lang.reflect.Method m] (not (contains? #{java.lang.Object clojure.lang.AFn java.lang.Iterable java.util.List java.util.Collection} (.getDeclaringClass m))))
+          (extract-methods non-static? clojure.lang.PersistentVector excluded-methods))
+  (fn [i] (mapv (fn [j] (* i j)) (range 1 (inc *wavefront-size*))))
+  input-fns)
 
 ;;------------------------------------------------------------------------------
 
