d066072b47fd4042b1f66f47aa303e397dbd89cc
Jules Gosnell <jules_gosnell@yahoo.com>
2014-05-20 20:18:27 +0100

    apply Tom's fix for recent ?regression?

--- test/clumatra/numbers_test.clj
+++ test/clumatra/numbers_test.clj
@@ -22,9 +22,6 @@
     (fetch-method Numbers "min" [Long/TYPE Double/TYPE])
     (fetch-method Numbers "num" [Double/TYPE])
     (fetch-method Numbers "num" [Float/TYPE])
-
-    (fetch-method Numbers "divide" [Long/TYPE Long/TYPE])
-
     })
 
 (def input-fns
