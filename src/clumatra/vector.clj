597a6eeb9be9721a576d3d81674827333a361641
Jules Gosnell <jules_gosnell@yahoo.com>
2014-05-21 19:40:13 +0100

    doh!

--- src/clumatra/vector.clj
+++ src/clumatra/vector.clj
@@ -263,7 +263,7 @@
           (if-let [src (aget array n)]
             (vector-node-to-array (+ offset (* m n)) new-shift src tgt)))))))
 
-(def thirty-two [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31])
+(def thirty-two (vec (range 32)))
 
 (defn vector-to-array [^clojure.lang.PersistentVector src]
   (let [length (.count src)
