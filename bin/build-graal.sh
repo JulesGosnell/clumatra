f3d75c88fe467198500bb5170f3ffe393d997dd7
jules <jules_gosnell@yahoo.com>
2014-05-25 11:46:07 +0100

    tighten up graal build script

--- bin/build-graal.sh
+++ bin/build-graal.sh
@@ -2,34 +2,19 @@
 
 ## distilled from: https://wiki.openjdk.java.net/display/Sumatra/Sumatra+JDK+build+instructions
 
-## as yourself:
-
-mkdir graal
-cd graal
-
-## build sumatra/jdk8
-export LANG=C
-export PATH=/usr/lib/jvm/java-openjdk/bin:$PATH
-hg clone http://hg.openjdk.java.net/sumatra/sumatra-dev
-cd sumatra-dev
-bash ./get_source.sh 
-bash ./configure
-make all
-cd ..
-
-## build graal and install in sumatra-dev tree
-export JAVA_HOME=`pwd`/sumatra-dev/build/linux-x86_64-normal-server-release/images/j2sdk-image/
-export EXTRA_JAVA_HOMES=/etc/alternatives/java_sdk_1.7.0
-hg clone http://hg.openjdk.java.net/graal/graal
-cd graal
-./mx.sh --vmbuild product --vm server build
-
-## test
+export LANG=C &&\
+export PATH=/usr/lib/jvm/java-openjdk/bin:$PATH &&\
+mkdir graal &&\
+cd graal &&\
+hg clone http://hg.openjdk.java.net/sumatra/sumatra-dev &&\
+cd sumatra-dev &&\
+bash ./get_source.sh  &&\
+bash ./configure &&\
+make all &&\
+cd .. &&\
+export JAVA_HOME=`pwd`/sumatra-dev/build/linux-x86_64-normal-server-release/images/j2sdk-image/ &&\
+export EXTRA_JAVA_HOMES=/etc/alternatives/java_sdk_1.7.0 &&\
+hg clone http://hg.openjdk.java.net/graal/graal &&\
+cd graal &&\
+./mx.sh --vmbuild product --vm server build &&\
 ./mx.sh --vm server unittest -XX:+TraceGPUInteraction -XX:+GPUOffload -G:Log=CodeGen hsail.test.IntAddTest
-
-## Note you must use the extra option -XX:+GPUOffload to enable offloading and
-## use -XX:+TraceGPUInteraction to see extra messages about GPU initialization etc.
-
-cd ../..
-
-
