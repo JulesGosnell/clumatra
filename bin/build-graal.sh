e85d056fd1cc2b554cdab10b0988db56029aa00b
jules <jules_gosnell@yahoo.com>
2014-06-21 21:44:36 +0100

    fix path

--- bin/build-graal.sh
+++ bin/build-graal.sh
@@ -3,6 +3,7 @@
 ## distilled from: https://wiki.openjdk.java.net/display/Sumatra/Sumatra+JDK+build+instructions
 
 export LANG=C && \
+export PATH=/usr/lib/jvm/java-openjdk/bin:$PATH &&\
 export JAVA_HOME=`pwd`/sumatra && \
 export EXTRA_JAVA_HOMES=/etc/alternatives/java_sdk_1.7.0 && \
 rm -rf graal jdk1.8.0-graal && \
@@ -10,4 +11,4 @@ hg clone http://hg.openjdk.java.net/graal/graal && \
 cd graal && \
 ./mx.sh --vmbuild product --vm server build && \
 ./mx.sh --vm server unittest -XX:+TraceGPUInteraction -XX:+GPUOffload -G:Log=CodeGen hsail.test.IntAddTest && \
-mv graal/graal/jdk1.8.0-internal/product jdk1.8.0-graal
+mv graal/jdk1.8.0-internal/product jdk1.8.0-graal
