fba4aefa65aaa13d66d8c7241784a97566e0df94
jules <jules_gosnell@yahoo.com>
2014-06-21 10:18:13 +0100

    continue separating sumatr and graal builds

--- bin/build-sumatra.sh
+++ bin/build-sumatra.sh
@@ -2,12 +2,13 @@
 
 ## distilled from: https://wiki.openjdk.java.net/display/Sumatra/Sumatra+JDK+build+instructions
 
-export LANG=C &&\
-export PATH=/usr/lib/jvm/java-openjdk/bin:$PATH &&\
-mkdir graal &&\
-cd graal &&\
-hg clone http://hg.openjdk.java.net/sumatra/sumatra-dev &&\
-cd sumatra-dev &&\
-bash ./get_source.sh  &&\
-bash ./configure &&\
-make all
+export LANG=C && \
+export PATH=/usr/lib/jvm/java-openjdk/bin:$PATH && \
+rm -rf sumatra-dev sumatra && \
+hg clone http://hg.openjdk.java.net/sumatra/sumatra-dev && \
+cd sumatra-dev && \
+bash ./get_source.sh  && \
+bash ./configure && \
+make all && \
+cd .. && \
+mv sumatra-dev/build/linux-x86_64-normal-server-release/images/j2sdk-image sumatra
