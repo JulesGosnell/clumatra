bee66a1a3aa192a9019a91646f97c49bc5b838b4
jules <jules_gosnell@yahoo.com>
2014-06-21 10:27:24 +0100

    continue separating sumatr and graal builds

--- bin/build-graal.sh
+++ bin/build-graal.sh
@@ -2,19 +2,12 @@
 
 ## distilled from: https://wiki.openjdk.java.net/display/Sumatra/Sumatra+JDK+build+instructions
 
-export LANG=C &&\
-export PATH=/usr/lib/jvm/java-openjdk/bin:$PATH &&\
-mkdir graal &&\
-cd graal &&\
-hg clone http://hg.openjdk.java.net/sumatra/sumatra-dev &&\
-cd sumatra-dev &&\
-bash ./get_source.sh  &&\
-bash ./configure &&\
-make all &&\
-cd .. &&\
-export JAVA_HOME=`pwd`/sumatra-dev/build/linux-x86_64-normal-server-release/images/j2sdk-image/ &&\
-export EXTRA_JAVA_HOMES=/etc/alternatives/java_sdk_1.7.0 &&\
-hg clone http://hg.openjdk.java.net/graal/graal &&\
-cd graal &&\
-./mx.sh --vmbuild product --vm server build &&\
-./mx.sh --vm server unittest -XX:+TraceGPUInteraction -XX:+GPUOffload -G:Log=CodeGen hsail.test.IntAddTest
+export LANG=C && \
+export JAVA_HOME=`pwd`/sumatra && \
+export EXTRA_JAVA_HOMES=/etc/alternatives/java_sdk_1.7.0 && \
+rm -rf graal jdk1.8.0-graal && \
+hg clone http://hg.openjdk.java.net/graal/graal && \
+cd graal && \
+./mx.sh --vmbuild product --vm server build && \
+./mx.sh --vm server unittest -XX:+TraceGPUInteraction -XX:+GPUOffload -G:Log=CodeGen hsail.test.IntAddTest && \
+mv graal/graal/jdk1.8.0-internal/product jdk1.8.0-graal
