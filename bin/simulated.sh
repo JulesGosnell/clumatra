c7d8c8ff1332956ab5ee2ee46e5b0f1cf87e9a15
jules <jules_gosnell@yahoo.com>
2014-06-22 10:55:01 +0100

    move download of jdk into build script

--- bin/simulated.sh
+++ bin/simulated.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -x
 
+if [ ! -d jdk1.8.0-graal ]
+then
+    wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/jdk1.8.0-graal.tgz | tar zxf -
+fi
+
 exec mvn -t conf/toolchains.xml -P simulated-acceleration clean compile clojure:test $@
