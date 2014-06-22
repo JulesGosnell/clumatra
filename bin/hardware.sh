7446f7268f5cca3910c57c99c90c54c3327a8861
jules <jules_gosnell@yahoo.com>
2014-06-22 10:56:07 +0100

    move download of jdk into build script

--- bin/hardware.sh
+++ bin/hardware.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -x
 
+if [ ! -d jdk1.8.0-graal ]
+then
+    wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/jdk1.8.0-graal.tgz | tar zxf -
+fi
+
+if [ ! -d lib ]
+then
+	mkdir lib
+	cd lib
+	wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libamdhsacl64.so
+	wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libhsakmt.so.1
+	wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libnewhsacore64.so
+	wget -nv https://raw.githubusercontent.com/HSAFoundation/Okra-Interface-to-HSA-Device/master/okra/dist/bin/libokra_x86_64.so
+	cd ..
+fi
+
 export CLUMATRA_HOME=`pwd`
 export PATH=$CLUMATRA_HOME/lib:$PATH
 export LD_LIBRARY_PATH=$CLUMATRA_HOME/lib
