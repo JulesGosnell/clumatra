8a8e463688d96bd09dbaff1da4dae7fc9edf1409
jules <jules_gosnell@yahoo.com>
2014-09-09 22:03:34 +0100

    add refresh flag to 'no' build

--- bin/no.sh
+++ bin/no.sh
@@ -8,10 +8,18 @@ do
     --junit)
 	    testgoal=test-with-junit
             ;;
+    --refresh)
+	    rm -fr jdk1.8.0-graal
+            ;;
     *)
 	    flags="$flags $arg"
             ;;
     esac
 done
+
+if [ ! -d jdk1.8.0-graal ]
+then
+    wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/jdk1.8.0-graal.tgz | tar zxf -
+fi
 
 exec mvn -t conf/toolchains.xml -P no-acceleration clean compile clojure:$testgoal $flags
