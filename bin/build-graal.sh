076ecc5e93f18d59e4d7d946f554fbfea919c9c2
Julian Gosnell <jules_gosnell@yahoo.com>
2015-02-13 23:44:50 +0000

    add refresh option, add graal switch, disable unit test (temporary?)

--- bin/build-graal.sh
+++ bin/build-graal.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -x
 
-## distilled from: https://wiki.openjdk.java.net/display/Sumatra/Sumatra+JDK+build+instructions
+## distilled from https://wiki.openjdk.java.net/display/Graal/Instructions
 
-export LANG=C && \
-export PATH=/usr/lib/jvm/java-openjdk/bin:$PATH &&\
-export JAVA_HOME=`pwd`/sumatra && \
-export EXTRA_JAVA_HOMES=/usr/local/java/jdk1.7.0_75 && \
+mkdir -p target
+cd target
+
+for arg in "$@"
+do
+    case "$arg" in
+        --refresh)
+            rm -fr sumatra
+            wget -nv -O- http://ouroboros.dyndns-free.com/clumatra/sumatra.tgz | tar zxf -
+            ;;
+    esac
+done
+
+# ./mx.sh --vm server unittest -XX:+TraceGPUInteraction -XX:+GPUOffload -G:Log=CodeGen hsail.test.IntAddTest && \
+
 rm -rf graal jdk1.8.0-graal && \
 hg clone http://hg.openjdk.java.net/graal/graal && \
 cd graal && \
-./mx.sh --vmbuild product --vm server build && \
-./mx.sh --vm server unittest -XX:+TraceGPUInteraction -XX:+GPUOffload -G:Log=CodeGen hsail.test.IntAddTest && \
+export JAVA_HOME=`pwd`/sumatra && \
+export EXTRA_JAVA_HOMES=/usr/local/java/jdk1.7.0_75 && \
+./mx.sh --vmbuild product --vm graal build && \
 cd .. && \
 mv graal/jdk1.8.0-internal/product jdk1.8.0-graal
