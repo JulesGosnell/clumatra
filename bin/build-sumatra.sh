908d01ac878a3dd6ececc6a7a25ddb338d76346a
jules <jules_gosnell@yahoo.com>
2015-02-13 21:08:29 +0000

    add /usr/local/bin to path for make-3.81 - fixes a backwards incompatibility in gmake

--- bin/build-sumatra.sh
+++ bin/build-sumatra.sh
@@ -3,7 +3,7 @@
 ## distilled from: https://wiki.openjdk.java.net/display/Sumatra/Sumatra+JDK+build+instructions
 
 export LANG=C && \
-export PATH=/usr/lib/jvm/java-openjdk/bin:$PATH && \
+export PATH=/usr/lib/jvm/java-openjdk/bin:/usr/local/bin:$PATH && \
 rm -rf sumatra-dev sumatra && \
 hg clone http://hg.openjdk.java.net/sumatra/sumatra-dev && \
 cd sumatra-dev && \
